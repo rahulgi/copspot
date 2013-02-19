f9ec6c026d900dcae748811e80fe3701b027cca0
Rahul Gupta-Iwasaki <rahulgi@stanford.edu>
2013-02-18 16:04:42 -0800

    Added code to track who posts and prevent people from posting more than once every hour.

--- controllers/spots.js
+++ controllers/spots.js
@@ -1,5 +1,6 @@
 
 var spots = [];
+var spotters = {};
 
 exports.index = function (req, res) {
   console.log (req);
@@ -8,15 +9,21 @@ exports.index = function (req, res) {
 
 exports.create = function (req, res) {
   console.log (req);
-  var indx = spots.length + 1;
-  spots.push ({
-    id : indx,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    userid : req.body.userid,
-    time : (new Date ()).getTime ()
-  });
-  res.send ("spot with id: " + indx + " created.");
+  var date = new Date ();
+  if (spotters[req.body.userid]) {
+    res.send ("HOURLY LIMIT MAXED. SPOT REJECTED.");
+  } else {
+    spotters[req.body.userid] = date.getTime ();
+    var indx = spots.length + 1;
+    spots.push ({
+      id : indx,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      userid : req.body.userid,
+      time : (new Date ()).getTime ()
+    });
+    res.send ("spot with id: " + indx + " created.");
+  }
 };
 
 /* Spots last for one hour, and we clean up old ones every 60 seconds. */
@@ -25,10 +32,20 @@ var spotDuration = 1000*60*60;
 
 var cleanSpots = function () {
   for (var i = 0; i < spots.length; i ++) {
-    if ((new Date()).getTime () - spots[i].time > spotDuration)
-      spots.splice (i, 1);
+    if ((new Date()).getTime () - spots[i].time > spotDuration) 
+      spots.splice (i--, 1);
   }
   setTimeout (cleanSpots, sleepInterval);
 };
 
+var cleanSpotters = function () {
+  var date = new Date ();
+  for (var spotter in spotters) {
+    if (date.getTime () - spotters[spotter] > spotDuration)
+      delete spotters[spotter];
+  }
+  setTimeout (cleanSpotters, sleepInterval);
+};
+
 cleanSpots ();
+cleanSpotters ();
